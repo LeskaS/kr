6db60f85ad6c02df6a4215e5df598b1499ce7919
Lesya <lesya413@mail.ru>
2024-10-10 23:54:31 +0300

    meow meow meow meow

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,18 +5,18 @@ class Program
     static void Main()
     {
         Console.Write("Введите трехзначное число: "); 
-        int num = int.Parse(Console.ReadLine());
-        if (num < 100 || num > 999) 
+        int num=int.Parse(Console.ReadLine());
+        if (num<100 || num>999) 
         {
             Console.WriteLine("Ошибка");
             Console.ReadKey();
-            return;
+            return; 
         }
-        int a = num % 10; // Единицы
-        int b = (num / 10) % 10; // Десятки
-        int c = num / 100; // Сотни
-        int sum = a + b + c; 
-        int pr = a * b * c;
+        int a= num%10; // Единицы
+        int b= (num/10)%10; // Десятки
+        int c= num /100; // Сотни
+        int sum= a+b+c; 
+        int pr= a*b*c;
         Console.WriteLine($"Число единиц: {a}");
         Console.WriteLine($"Число десятков: {b}");
         Console.WriteLine($"Сумма цифр: {sum}");
